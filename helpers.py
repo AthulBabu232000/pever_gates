59c4822f7a873167d11e4d3d04f453ea07e0295d
mahadevananair1 <mahadevananair@gmail.com>
2020-10-17 03:30:46 +0530

    Take it EC Lab Using Flask Server ALL Sensitive User Data is hashed or encrypted Based on python Flask server It enables users to have their own profile page and record of assignments and experiments.
     Installation
    
    Use the package manager [pip] to install flask and necessary items as per REQUIREMENTS.txt
    
     Usage
    
    $ Flask run
    
     Contributing
    Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
    
    Please make sure to update tests as appropriate.
    https://github.com/mahadevananair1   :)
     License
    [MIT](https://choosealicense.com/licenses/mit/)

---
+++ helpers.py
@@ -0,0 +1,26 @@
+import os
+import requests
+import urllib.parse
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    return render_template("apology.html", top=code, bottom=message)
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")    
+        return f(*args, **kwargs)
+    return decorated_function
+
